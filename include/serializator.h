9f357e4288757dfde00194d2986af2e8f940224f
dicobi <dicobi291@gmail.com>
2024-04-26 09:27:16 +0300

    Added trivial deserialization.

--- include/serializator.h
+++ include/serializator.h
@@ -20,8 +20,14 @@ void serialize(std::vector<std::byte> &buf, const DataType dataType, const T& da
 
 	auto dataSize = sizeof(data);
 	buf.resize(buf.size() + dataSize + 1);
-	std::memcpy(buf.data() + (buf.size() - dataSize - 1), reinterpret_cast<std::byte*>(dataType), sizeof(dataType));
-	std::memcpy(buf.data() + (buf.size() - dataSize), reinterpret_cast<std::byte*>(data), dataSize);
+	std::memcpy(buf.data() + (buf.size() - dataSize - 1), reinterpret_cast<const std::byte*>(&dataType), sizeof(dataType));
+	std::memcpy(buf.data() + (buf.size() - dataSize), reinterpret_cast<const std::byte*>(&data), dataSize);
+}
+
+template<typename T, typename = std::enable_if_t<std::is_fundamental_v<T> > >
+void deserialize(const std::byte *bufStart, T &data)
+{
+	std::memcpy(reinterpret_cast<std::byte *>(&data), bufStart, sizeof(data));
 }
 
 } //binary_serializator
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,33 @@ int main()
 	std::vector<std::byte> buff;
 
 	serialize(buff, DataType::INT, 156);
-	serialize(buff, DataType::FLOAT, 234.86);
+	serialize(buff, DataType::FLOAT, 234.86f);
 	serialize(buff, DataType::INT, 2356);
+
+	auto dataType {DataType::UNKNOWN};
+	for (int i = 0; i < buff.size();) {
+		dataType = static_cast<DataType>(buff[i]);
+		switch (dataType) {
+			case DataType::INT:
+			{
+				int val {0};
+				deserialize(buff.data() + i + 1, val);
+				i += sizeof(val) + 1;
+				std::cout << "Deserialized int val: " << val << std::endl;
+				break;
+			}
+			case DataType::FLOAT:
+			{
+				float val {0};
+				deserialize(buff.data() + i + 1, val);
+				i += sizeof(val) + 1;
+				std::cout << "Deserialized float val: " << val << std::endl;
+				break;
+			}
+			default:
+				break;
+		}
+	}
 
 	return 0;
 }
